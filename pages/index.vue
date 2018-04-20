1564a46951637bf3404602b06fae97e68a0b2110
mrsteele <matthewsteele1@hotmail.com>
2018-04-20 19:44:53 -0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Hello {{ name }}!</h1>
+    <p>async? {{ test }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return { name: 'world' }
+  },
+  asyncData: () => {
+    return {
+      test: 'What?!'
+    }
+  },
+  head () {
+    return {
+      title: 'hello!'
+    }
+  }
+}
+</script>
---
+++ pages/todo.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <h1>Todo</h1>
+    <ul class="todos">
+      <li v-for="todo in todos" v-bind:class="{ completed: todo.complete }" v-on:click="toggle(todo.id)">
+        {{ todo.text }}
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style>
+.todos li { cursor: pointer; }
+.todos li:hover { color: blue; }
+.completed { text-decoration: line-through; }
+</style>
+
+<script>
+export default {
+  data: () => ({
+    todos: [{
+      id: 1,
+      complete: false,
+      text: 'first thing'
+    }, {
+      id: 2,
+      complete: false,
+      text: 'second thing'
+    }, {
+      id: 3,
+      complete: false,
+      text: 'third'
+    }]
+  }),
+  methods: {
+    toggle: function (id) {
+      const todo = this.todos.find(todo => todo.id === id)
+      if (todo) {
+        console.log('here', todo)
+        todo.complete = !todo.complete
+      }
+    }
+  },
+  head () {
+    return {
+      title: 'Todo Example'
+    }
+  }
+}
+</script>
